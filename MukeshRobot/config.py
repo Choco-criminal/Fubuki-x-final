f90c0f90741e97abf17bd44e004a746c245409cb
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-07 14:10:41 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,16 +1,16 @@
 class Config(object):
     LOGGER = True
-    API_ID =None 
-    API_HASH = ""
-    TOKEN = ""  
-    OWNER_ID=None
+    API_ID =25475489 
+    API_HASH = "3fc2b371f4fbb0166758736414d8be92"
+    TOKEN = "7617090520:AAH6OqlzkAGlJUhtd6O3yd9sRfDo6lxwn00"  
+    OWNER_ID=1266240012
     
-    SUPPORT_CHAT = "" 
-    START_IMG = ""
-    EVENT_LOGS = ()
-    MONGO_DB_URI= ""
+    SUPPORT_CHAT = "mylove2958" 
+    START_IMG = "https://envs.sh/pC0.mp4"
+    EVENT_LOGS = (-1002122538649)
+    MONGO_DB_URI= "mongodb+srv://Choco:Choco@cluster0.yddf3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
    
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://avnadmin:AVNS_TbzxTp8QTaLOy_GjvCr@choco-chocoxgithub-f883.j.aivencloud.com:16510/defaultdb?sslmode=require"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""
     )
